faa2350b3e2aa6eb2daef8247c4193b2c3340b2a
Rohit Ranjan <arohitranjan11@gmail.com>
2021-09-23 02:13:23 +0530

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function Banner() {
+    return (
+        <div className='banner'>
+        <h1> Maa ki </h1>
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+    <div className="header">
+     {/* <h1>Hey this is the main</h1> */}
+        <div class="ipl-ph">
+            <img className="ipl" src= 'https://wallpapercave.com/wp/wp4059913.jpg' alt="IPL Header"/>
+        </div>        
+    </div>
+    );
+}
+
+export default Header
